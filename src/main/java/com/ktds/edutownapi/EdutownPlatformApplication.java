ec54a3ac5f9615854cc55f947239090f515f1a5b
82256977 <chanho.jeong@kt.com>
2022-09-15 13:53:18 +0900

    EduTown init

---
+++ src/main/java/com/ktds/edutownapi/EdutownPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.edutownapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutownPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutownPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktds/edutownapi/EdutownPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.edutownapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutownPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
